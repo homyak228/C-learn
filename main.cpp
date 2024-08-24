2cf08bb001fce2c0f48f820bf4a6085060ace746
homyak228 <gia.100811@gmail.com>
2024-08-24 19:26:36 +0300

    Beleberda generator

--- main.cpp
+++ main.cpp
@@ -4,7 +4,18 @@ int main()
 {
 	setlocale(LC_ALL, "RU");
 
-	std::cout << "Привет, мир!" << std::endl;
+	std::string adj = "";
+	std::string verb = "";
+	std::string noun = "";
+
+	std::cout << "Привет, введи любое существительное:" << std::endl;
+	std::cin >> noun;
+	std::cout << "Красава, а теперь введи любое прилагательное тогоже пола, что и существительное:" << std::endl;
+	std::cin >> adj;
+	std::cout << "Молодец, ну а сейчас мне нужен любой глагол в настоящем времени." << std::endl;
+	std::cin >> verb;
+
+	std::cout << "Вот, что у тебя получилось:" << '\n' << adj << " " << noun << " " << verb << " как одержимый(ая)!" << std::endl;
 
 	return 0;
 }
